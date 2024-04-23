2791543351126914c0505d4462433c8d9f320555
Simonedambra <simonedambra130102@gmail.com>
2024-04-23 14:24:10 +0200

    [upd] bonus random number

--- myscrips.js
+++ myscrips.js
@@ -6,7 +6,7 @@ const username = prompt('inserisci il tuo nome');
 const userlastname = prompt('inserisci il tuo cognome');
 const color = prompt('inserisci il tuo colore preferito');
 
-let correntext = username + '' + userlastname + '' + color + '21';
+let correntext = username + '' + userlastname + '' + color + Math.round(Math.random() * 100);
 
 console.log(correntext)
 
