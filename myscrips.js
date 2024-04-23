169e03976264b1587c2a00bb7cf71c5332adc603
Simonedambra <simonedambra130102@gmail.com>
2024-04-23 14:06:33 +0200

    first commit

---
+++ myscrips.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let text1 = document.getElementById("text");
+
+const username = prompt('inserisci il tuo nome');
+const userlastname = prompt('inserisci il tuo cognome');
+const color = prompt('inserisci il tuo colore preferito');
+
+let correntext = username + '' + userlastname + '' + color + '21';
+
+text1.innerHTML = correntext;
