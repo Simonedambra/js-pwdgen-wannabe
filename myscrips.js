eafc8dbc61bd41d77e1c83fcd2ebacee8b79aa3c
Simonedambra <simonedambra130102@gmail.com>
2024-04-23 14:35:24 +0200

    [upd] added `` in correntext

--- myscrips.js
+++ myscrips.js
@@ -5,8 +5,12 @@ let text1 = document.getElementById("text");
 const username = prompt('inserisci il tuo nome');
 const userlastname = prompt('inserisci il tuo cognome');
 const color = prompt('inserisci il tuo colore preferito');
+const number = Math.round(Math.random() * 100);
 
-let correntext = username + '' + userlastname + '' + color + Math.round(Math.random() * 100);
+let correntext = `
+${username} ${userlastname} ${color} ${number} 
+`
+// let correntext = username + '' + userlastname + '' + color + Math.round(Math.random() * 100);
 
 console.log(correntext)
 
